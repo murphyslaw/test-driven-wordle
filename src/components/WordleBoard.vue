8c1810111af8f847481bc2d9416091aa39b08cc8
Falk Köppe <falk.koeppe@gmail.com>
2024-01-12 20:45:07 +0100

    Do not render non-letter characters

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import englishWords from '@/englishWordsWith5Letters.json'
 import { DEFEAT_MESSAGE, VICTORY_MESSAGE, WORD_SIZE } from '@/settings'
-import { computed, ref } from 'vue'
+import { ref, watchEffect } from 'vue'
 
 defineProps({
   wordOfTheDay: {
@@ -13,16 +13,11 @@ defineProps({
 const guessInProgress = ref('')
 const guessSubmitted = ref('')
 
-const formattedGuessInProgress = computed({
-  get() {
-    return guessInProgress.value
-  },
-  set(value: string) {
-    guessInProgress.value = value
-      .slice(0, WORD_SIZE)
-      .toUpperCase()
-      .replace(/[^A-Z]+/gi, '')
-  }
+watchEffect(async () => {
+  guessInProgress.value = guessInProgress.value
+    .slice(0, WORD_SIZE)
+    .toUpperCase()
+    .replace(/[^A-Z]+/gi, '')
 })
 
 function onSubmit() {
@@ -33,14 +28,40 @@ function onSubmit() {
 </script>
 
 <template>
-  <input
-    v-model="formattedGuessInProgress"
-    type="text"
-    :maxlength="WORD_SIZE"
-    @keydown.enter="onSubmit"
-  />
-  <p
-    v-if="guessSubmitted.length > 0"
-    v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE"
-  ></p>
+  <main>
+    <input v-model="guessInProgress" :maxlength="WORD_SIZE" type="text" @keydown.enter="onSubmit" />
+
+    <p
+      v-if="guessSubmitted.length > 0"
+      class="end-of-game-message"
+      v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE"
+    />
+  </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 3rem;
+}
+
+.end-of-game-message {
+  font-size: 3rem;
+  animation: end-of-game-message-animation 700ms forwards;
+  white-space: nowrap;
+  text-align: center;
+}
+
+@keyframes end-of-game-message-animation {
+  0% {
+    opacity: 0;
+    transform: rotateZ(0);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(2rem);
+  }
+}
+</style>
--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -85,5 +85,12 @@ describe('WordleBoard', () => {
 
       expect(wrapper.find<HTMLInputElement>('input[type=text]').element.value).toEqual('TST')
     })
+
+    test('non-letter characters do not render on the screen while being typed', async () => {
+      await playerSubmitsGuess('12')
+      await playerSubmitsGuess('123')
+
+      expect(wrapper.find<HTMLInputElement>('input[type=text]').element.value).toEqual('')
+    })
   })
 })
