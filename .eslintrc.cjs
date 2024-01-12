e358453f29de2bd611134ab9f1694bee709200f5
Falk Köppe <falk.koeppe@gmail.com>
2024-01-12 11:36:20 +0100

    update lint and format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,19 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
+  env: {
+    node: true
+  },
+  extends: [
     'eslint:recommended',
+    'plugin:vue/vue3-recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    'prettier'
   ],
+  plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': ['error']
   }
 }
